fe8ff2bcf01891378130ca2bf3bc7819f8974970
joeswagson <uranousbridge@gmail.com>
2025-11-04 21:48:51 -0800

    charset sanitization for strings

--- SilkBound/Extensions/StringExtensions.cs
+++ SilkBound/Extensions/StringExtensions.cs
@@ -1,16 +1,27 @@
 ﻿using SilkBound.Behaviours;
 using SilkBound.Network;
 using SilkBound.Utils;
+using System.Linq;
 
-namespace SilkBound.Extensions
-{
-    public static class StringExtensions
-    {
-        public static string ReplaceController(this string path, Weaver? target=null)
+namespace SilkBound.Extensions {
+    public static class CharacterSets {
+        public static char[] ATOZLOWER = [.. "abcdefghijklmnopqrstuvwxyz"];
+        public static char[] ATOZUPPER = [.. "ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+        public static char[] ATOZ = [.. ATOZLOWER, .. ATOZUPPER];
+
+        public static char[] NUMBERS = [.. "0123456789"];
+        public static char[] SPECIAL = [.. "!@#$%^&*()"];
+
+        public static char[] ALPHANUMERIC = [.. ATOZ, .. NUMBERS];
+        public static char[] PRINTABLE = [.. ALPHANUMERIC, .. SPECIAL];
+    }
+    public static class StringExtensions {
+        public static string ReplaceController(this string path, Weaver? target = null)
         {
             target ??= NetworkUtils.LocalClient;
-            
+
             return path.Replace("Hero_Hornet(Clone)", HornetMirror.GetObjectName(target.ClientID));
         }
+        public static string Sanitize(this string src, char[] allowed) => new string([.. src.Where(c => allowed.Contains(c))]);
     }
 }
