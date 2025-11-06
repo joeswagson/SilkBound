0779b9533a79bfa6e4a75a59a0a209ff0ea96a90
Joe Swanson <uranousbridge@gmail.com>
2025-11-06 15:03:19 -0800

    Incomplete patch structure for recoil cancellation. Will continue @ host machine.

---
+++ SilkBound/Patches/Simple/Attacks/NailSlashRecoilPatches.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SilkBound.Patches.Simple.Attacks {
+    [HarmonyPatch(typeof(NailSlashRecoil))]
+    public class NailSlashRecoilPatches {
+        //HitResponse(DamageEnemies.HitResponse response)
+    }
+}
--- SilkBound/Utils/NetworkStats.cs
+++ SilkBound/Utils/NetworkStats.cs
@@ -9,23 +9,23 @@ namespace SilkBound.Utils {
     public struct NetworkStats(NetworkConnection connection) {
         public NetworkConnection Connection = connection;
 
-        public int PacketsSent = 
-        public int PacketsDropped = 0;
+        //public int PacketsSent = 
+        //public int PacketsDropped = 0;
 
-        internal void HandlePacketTask(Task packetSend, byte[] data)
-        {
-            if (packetSend.IsCompletedSuccessfully)
-                Packents
-            else
-                        PacketDropped(data);
-        }
-        public void PacketSent(byte[] data)
-        {
+        //internal void HandlePacketTask(Task packetSend, byte[] data)
+        //{
+        //    if (packetSend.IsCompletedSuccessfully)
+        //        Packents
+        //    else
+        //                PacketDropped(data);
+        //}
+        //public void PacketSent(byte[] data)
+        //{
 
-        }
-        public void PacketDropped(byte[] data)
-        {
+        //}
+        //public void PacketDropped(byte[] data)
+        //{
 
-        }
+        //}
     }
 }
