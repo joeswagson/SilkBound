185e5c776d9373b470fb916a69798b10e9a69514
Joe Swanson <uranousbridge@gmail.com>
2025-11-06 11:16:29 -0800

    Shallows class for empty generators.

---
+++ SilkBound/Utils/Shallows.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SilkBound.Utils {
+    public static class Shallows {
+        public static IEnumerator Coroutine()
+        {
+            yield break;
+        }
+
+        public static IEnumerator<T> Coroutine<T>()
+        {
+            yield break;
+        }
+    }
+}
