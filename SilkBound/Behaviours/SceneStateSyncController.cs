0f6953c74ba07db9e95540649da8b7202cfe9987
joeswagson <uranousbridge@gmail.com>
2025-11-06 16:49:38 -0800

    Fix scene state syncing throwing without server

--- SilkBound/Behaviours/SceneStateSyncController.cs
+++ SilkBound/Behaviours/SceneStateSyncController.cs
@@ -11,22 +11,24 @@ namespace SilkBound.Behaviours {
         static bool firstLoad = true;
 
         public static event Action<Scene>? SceneLoaded;
+        
         private void Update() {
-            if (activated)
+            if (activated || !NetworkUtils.Connected)
                 return;
+
+            activated = true;
 
             var scene = SceneManager.GetActiveScene();
             if (SilkConstants.DEBUG && scene.name == "Tut_03")
-                GameObject.Find("RestBench").transform.position.Set(76.8323f, 17.5686f, 0.004f);
+                GameObject.Find("RestBench").transform.position = new Vector3(76.8323f, 17.1686f, 0.004f);
             if (scene.name != "Menu_Title" && scene.name != "Pre_Menu_Title")
                 firstLoad = false;
-            activated = true;
 
             SceneLoaded?.Invoke(scene);
             Logger.Msg("syncing scenestate:", scene.name);
             Logger.Msg("- clients in scene:", Server.CurrentServer.GetPlayersInScene());
 
-            if (Server.CurrentServer.GetPlayersInScene() == 0)
+            if (Server.CurrentServer.GetPlayerCountInScene() == 0)
                 SceneStateManager.ProposeChanges(scene.name, StateChange.Reset());
 
             SceneStateManager.Fetch(scene.name).Result.Sync(scene);
