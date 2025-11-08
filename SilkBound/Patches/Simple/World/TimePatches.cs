d5acd2831590d2e7b65b3419e1f0e0244519bb2c
joeswagson <uranousbridge@gmail.com>
2025-11-07 17:22:40 -0800

    Stop unregulated timescale updates.

---
+++ SilkBound/Patches/Simple/World/TimePatches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using SilkBound.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SilkBound.Patches.Simple.World {
+    [HarmonyPatch(typeof(Time))]
+    public class TimePatches {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(Time.timeScale), MethodType.Setter)]
+        public static bool Prefix(ref float value)
+        {
+            if (!NetworkUtils.Connected) return true;
+
+            value = 1f;
+            return false;
+        }
+    }
+}
