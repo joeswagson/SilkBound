c484b5e35164de4c416b1f1e35ff5ea638cf9e92
joeswagson <uranousbridge@gmail.com>
2025-11-07 17:19:20 -0800

    remove path props

--- SilkBound/Generated/Props.template.cs
+++ SilkBound/Generated/Props.template.cs
@@ -4,11 +4,6 @@ using System.Text;
 
 namespace SilkBound.Generated {
     internal class Props {
-        internal const string ONLINE_PATH = @"$(SilksongPath_Steam)";
-        internal const string OFFLINE_PATH = @"$(SilksongPath_Offline)";
-
-        internal const string LOCAL_PROPS = @"$(LocalPropsDir)";
-
         internal const bool SMALL_WINDOW = $([MSBuild]::ValueOrDefault($(SmallWindow), false));
     }
 }
