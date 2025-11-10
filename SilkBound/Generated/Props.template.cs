e2910a42931f14f7cbe8818bc3354c007bf56504
joeswagson <uranousbridge@gmail.com>
2025-11-10 14:05:48 -0800

    Additional flags for local.props.

--- SilkBound/Generated/Props.template.cs
+++ SilkBound/Generated/Props.template.cs
@@ -5,5 +5,10 @@ using System.Text;
 namespace SilkBound.Generated {
     internal class Props {
         internal const bool SMALL_WINDOW = $([MSBuild]::ValueOrDefault($(SmallWindow), false));
+        internal const bool DRAW_COLLIDERS = $([MSBuild]::ValueOrDefault($(DrawColliders), false));
+        internal const bool DEBUG = $([MSBuild]::ValueOrDefault($(Debug), false));
+        internal const bool IMMORTAL = $([MSBuild]::ValueOrDefault($(Immortal), false));
+        internal const bool POWERUPS = $([MSBuild]::ValueOrDefault($(Powerups), false));
+        internal const int MONITOR = $([MSBuild]::ValueOrDefault($(Monitor), false));
     }
 }
--- SilkBound/Utils/SilkConstants.cs
+++ SilkBound/Utils/SilkConstants.cs
@@ -1,21 +1,24 @@
-﻿namespace SilkBound.Utils
+﻿using SilkBound.Managers;
+using System;
+
+namespace SilkBound.Utils
 {
     // netent = NetworkEntity
 
     public class SilkConstants
     {
-        public const bool DEBUG =
+        public static readonly bool DEBUG = 
             #if DEBUG
                 true
             #else
-                false
+                LocalProps.Safe<bool>("DEBUG")
             #endif
         ;
-        public const bool DEBUG_COLLIDERS = false;
-        public const short TEST_CLIENTS = 1;
+        public static readonly bool DEBUG_COLLIDERS = LocalProps.Safe<bool>("DRAW_COLLIDERS");
+        public const short TEST_CLIENTS = 2;
         #region debug cheats
-        public const bool INVULNERABILITY = false;// DEBUG;
-        public const bool GETALLPOWERUPS = DEBUG;
+        public static readonly bool INVULNERABILITY = LocalProps.Safe<bool>("IMMORTAL");// DEBUG
+        public static readonly bool GETALLPOWERUPS = LocalProps.Safe<bool>("POWERUPS");
         #endregion
 
         public const bool CUSTOM_TITLE = true;
@@ -23,10 +26,10 @@
         public const short MAX_CLIENTS = 100; // TODO: never implement this (2 billion people need to play together)
         public const short MAX_NAME_LENGTH = 20;
 
-        public const ushort PORT = 5000;
+        public const ushort PORT = 30300;
         public const short PACKET_BUFFER = 512;
         public const int CONNECTION_TIMEOUT = 5000; // ms
-        public const short CHUNK_TRANSFER = PACKET_BUFFER - byte.MaxValue - 16 - 1;
+        public const short CHUNK_TRANSFER = PACKET_BUFFER - sizeof(ushort) - 16 - 1; 
 
         public class Server
         {
