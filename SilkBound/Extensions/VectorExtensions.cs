f78aa94f310a41dd681a7478e8a981289cdd4c7e
joeswagson <uranousbridge@gmail.com>
2025-11-13 19:07:57 -0800

    Flattening/expanding methods for unity vectors.

---
+++ SilkBound/Extensions/VectorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SilkBound.Extensions {
+    public static class VectorExtensions {
+
+        #region Vector3
+
+        public static Vector2 Transform(this Vector3 vec)
+        {
+            return new Vector2(vec.x, vec.y);
+        }
+
+        #endregion
+
+        #region Vector2
+
+        public static Vector3 Transform(this Vector2 vec, float layer = 0f)
+        {
+            return new Vector3(vec.x, vec.y, layer);
+        }
+
+        #endregion
+
+    }
+}
