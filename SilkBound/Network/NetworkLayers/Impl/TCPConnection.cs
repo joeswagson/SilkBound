fc2109b740030b56f871abd5c6c02f442d5721b3
Joe Swanson <uranousbridge@gmail.com>
2025-12-15 13:04:34 -0800

    TCPConnections no longer checks for an existing connections before finally returning true for IsConnected

--- SilkBound/Network/NetworkLayers/Impl/TCPConnection.cs
+++ SilkBound/Network/NetworkLayers/Impl/TCPConnection.cs
@@ -27,7 +27,7 @@ namespace SilkBound.Network.NetworkLayers.Impl
 
         public bool HasConnection { get; private set; }
 
-        public override bool IsConnected => _client != null && _client.Connected;
+        public override bool IsConnected => _client != null;// && _client.Connected;
 
         public TCPConnection(string host, int? port = null) : base(new ClientPacketHandler(), host, port)
         {
