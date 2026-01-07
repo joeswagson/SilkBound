387db4d96b9b724a2080aaf4ffba8393706395a7
Joe Swanson <uranousbridge@gmail.com>
2026-01-07 09:23:19 -0800

    Base for field when migrating NetworkEntity health from a field to an object.

---
+++ SilkBound/Sync/ReplicatedHealth.cs
@@ -0,0 +1,24 @@
+﻿using SilkBound.Network;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SilkBound.Sync {
+    public class ReplicatedHealth {
+        private float hp;
+        public float Health => hp;
+
+        private NetworkEntity? Entity;
+        public void UpdateHost(NetworkEntity entity) => Entity = entity;
+        public void TakeDamage(Weaver source, HitInstance hit)
+        {
+            if (Entity == null)
+                return;
+
+            if (Entity.Owner != source) {
+
+                return;
+            }
+        }
+    }
+}
